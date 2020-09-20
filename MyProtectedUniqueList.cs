6cf4c9c0b787781d47c66b6a79bd89643dce05ec
LironBayda <34834883+LironBayda@users.noreply.github.com>
2020-09-20 03:51:28 +0300

    Add files via upload

---
+++ MyProtectedUniqueList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProtectedUniqueList15092020
+{
+    class MyProtectedUniqueList<T>: IEnumerable<T>
+    {
+        private List<T> _words;
+        string _secret_word;
+        public MyProtectedUniqueList(string secret_word )
+        {
+            _secret_word = secret_word;
+            
+                _words = new List<T>();
+            
+        }
+
+        public void Add(T word)
+        {
+            if (word == null)
+            {
+                throw new ArgumentNullException("your word is null"); ;
+            }
+            else if (wordInWords(word))
+            {
+                throw new InvalidOperationException("the ward that you gave alraedy exist");
+            }
+            _words.Add(word);
+        }
+
+
+        public void Remove(T word)
+        {
+            if (word == null)
+            {
+                throw new ArgumentNullException("your word is null"); ;
+            }
+            else if (!wordInWords(word))
+            {
+                throw new ArgumentException("the ward that you gave isnwt exist");
+            }
+            _words.Remove(word);
+        }
+
+        public void RemoveAt(int ind)
+        {
+            if (ind<0)
+            {
+                throw new ArgumentOutOfRangeException("index small then zero"); ;
+            }
+            else if (ind>_words.Count)
+            {
+                throw new ArgumentOutOfRangeException("ind larger then List size");
+            }
+            _words.RemoveAt(ind);
+        }
+
+        public void Clear(string key)
+        {
+            if (key == _secret_word)
+                _words.Clear();
+            else
+                throw new AccessViolationException("the key is worng");
+
+        }
+
+        public void Sort(string key)
+        {
+            if (key == _secret_word)
+                _words.Sort();
+            else
+                throw new AccessViolationException("the key is worng");
+
+        }
+
+        public override string ToString()
+        {
+            string str = " ";
+            _words.ForEach(word => str+= word+" ");
+            
+            return base.ToString()+str;
+
+        }
+
+        private bool wordInWords(T word) 
+        {
+            foreach (T _word in _words)
+            {
+                if (_word.Equals(word))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _words.GetEnumerator();  
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _words.GetEnumerator();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProtectedUniqueList15092020
+{
+    class Program
+    {
+        static MyProtectedUniqueList<string> myProtectedUniqueList = new MyProtectedUniqueList<string>("gali");
+
+        static void Add(string word)
+        {
+            try
+            {
+                myProtectedUniqueList.Add(word);
+            }
+
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+        }
+
+        static void Remove(string word)
+        {
+            try
+            {
+                myProtectedUniqueList.Remove(word);
+            }
+
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        static void RemoveAt(int ind)
+        {
+            try
+            {
+                myProtectedUniqueList.RemoveAt(ind);
+            }
+
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Clear(string word)
+        {
+            try
+            {
+                myProtectedUniqueList.Clear(word);
+            }
+
+            catch (AccessViolationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+        }
+
+        static void Sort(string word)
+        {
+            try
+            {
+                myProtectedUniqueList.Sort(word);
+            }
+
+            catch (AccessViolationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Add(null);
+
+          
+            Add("danny");
+            Add("tool");
+            Add("cookie");
+            Add("cookie");
+            Add("remove");
+            Add("remove");
+
+            Remove("remove");
+            Remove("remove");
+            RemoveAt(-9);
+            RemoveAt(60);
+            Clear("poli");
+            Sort("dana");
+            Sort("gali");
+
+            Console.WriteLine(myProtectedUniqueList.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
